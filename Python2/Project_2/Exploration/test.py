b600195bb8325f27f8a6db5b5b78379bef99e99a
Duncan Ferguson <Duncan.Ferguson@du.edu>
2021-01-28 13:55:02 -0700

    1:54 commit try

---
+++ Python2/Project_2/Exploration/test.py
@@ -0,0 +1,3 @@
+import base
+
+base.fib(1000)
---
+++ Python2/Project_2/test_count.py
@@ -0,0 +1,6 @@
+# Student: Duncan Ferguson
+# Student ID: 871-641-260
+# Class: COMP-3006-2; Assignment 2: Counting Characters
+# Date: 2/4/2021
+# Teacher: dalton.crutchfield@du.edu
+# TA: sunny.shrestha@du.edu
