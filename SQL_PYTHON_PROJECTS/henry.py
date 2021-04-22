e64533a5f820cf1614f5cea812851637dd9081f3
Duncan Ferguson <duncan.ferguson@du.edu>
2021-04-21 23:41:32 -0600

    Need to switch to tabs

--- SQL_PYTHON_PROJECTS/henry.py
+++ SQL_PYTHON_PROJECTS/henry.py
@@ -17,10 +17,10 @@ class bookstore(tk.Frame):
         self.parent.title("Henry's Bookstore")
         self.parent.geometry("700x700")
         # TODO make label look better
-        self.label=tk.Label(self.parent, text="Type of Search").pack()
+        self.label = tk.Label(self.parent, text="Type of Search").pack()
         self.cb = StringVar()
         self.b1 = tk.Checkbutton(self.parent, text="Author", variable=self.cb, onvalue='author', offvalue="").pack()
-        self.b2 = tk.Checkbutton(self.parent, text="Category", variable=self.cb, onvalue='category', offvalue="").pack()
+        # self.b2 = tk.Checkbutton(self.parent, text="Category", variable=self.cb, onvalue='category', offvalue="").pack()
         self.b3 = tk.Checkbutton(self.parent, text="Publisher", variable=self.cb, onvalue='publisher', offvalue="").pack()
         self.submit_button = tk.Button(self.parent, text="Submit", state=NORMAL, command=self.submitted, padx=20, pady=5).pack()
 
@@ -42,20 +42,29 @@ class DBTest():
         self.mydb.commit()
         self.mydb.close()
 
-    def getbooks(self):
-        sql = "SELECT * FROM henry_author"
+    def getbooks(self,selection):
+        # TODO only works for author and publisher
+        sql = "SELECT * FROM henry_"+selection
         self.mycur.execute(sql)
 
         for row in self.mycur:
             print(row[1], " ,", row[2])
 
 
+def runsql(run):
+    """This function runs the query"""
+    test = DBTest()
+    test.getbooks(run.cat_choice)
+    test.close()
+
+
 if __name__ == '__main__':
    root = tk.Tk()
    run = bookstore(root)
    root.mainloop()
+   runsql(run)
 
-   test = DBTest()
-   test.getbooks()
-   test.close()
+   # test = DBTest()
+   # test.getbooks()
+   # test.close()
 
