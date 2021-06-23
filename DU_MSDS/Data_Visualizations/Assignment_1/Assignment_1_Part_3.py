156e88ddc2c245c4baab5cdd796cca5935af8205
Duncan Ferguson <duncan.ferguson@du.edu>
2021-06-22 23:25:00 -0600

    Part 3 Complete

--- DU_MSDS/Data_Visualizations/Assignment_1/Assignment_1_Part_3.py
+++ DU_MSDS/Data_Visualizations/Assignment_1/Assignment_1_Part_3.py
@@ -5,7 +5,7 @@
 # Date 6/24/2021
 
 """
-• Assignment 1, Part 3: Write a program in Python to create and print out the first
+Assignment 1, Part 3: Write a program in Python to create and print out the first
 12 Fibonacci numbers. Then iterate over the last five numbers to build another
 list with the ratio of each number to its predecessor. What do you observe about
 this latter list?
@@ -21,15 +21,19 @@ def fib(n):
 fib_list = []
 
 # Creating the first 12 values of the fibonacci numbers not including 0
-for i in range(1,13,1): fib_list.append(fib(i))
+for i in range(1, 13, 1):
+    fib_list.append(fib(i))
 
-# Printing out the first 12 values of the fibonacci numbers
-for i in range(len(fib_list)): print(fib_list[i])
+print("First 12 Fibonacci Numbers: ", fib_list)
+
+# Then iterate over the last five numbers to build another
+# list with the ratio of each number to its predecessor.
 
 ratio_list = []
 
-for i in range(7,12,1):
+for i in range(7, 12, 1):
     ratio_list.append(fib_list[i]/fib_list[i-1])
 
-print(ratio_list)
+print("Last Fivenumbers", ratio_list)
 
+print("Observations about the ratio list is that starting to converge onto Phi, the Golden Ratio")
