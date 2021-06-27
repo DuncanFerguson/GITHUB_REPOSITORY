619b80a6541299e4f86fb73c447db138b15c682e
Duncan Ferguson <duncan.ferguson@du.edu>
2021-06-27 16:59:55 -0600

    Lab 3 In progress, working on the factoring

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_3.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_3.py
@@ -20,22 +20,31 @@ def isPrime(p):
 def nBitPrime(n):
     """Creating a Random Prime Number n-bits long"""
     num = int(2**n * random.random())  # Random Number n bits long
-    if num >= 2:
-        if isPrime(num):
-            return int(num)
-        else:
-            nBitPrime(n)
-    else:
-        nBitPrime(n)
+    while not isPrime(num):
+        num = int(2 ** n * random.random())
+    return num
+
 
 def factor(pq):
     """ Factoring out Pq"""
     for i in range(2,pq,1):
-        if (p % i) == 0:
-            print("Prime!")
+        if (pq % i) == 0 and isPrime(i) and isPrime(int(pq/i)):
+            p = i
+            q = pq/p
+            return(p,q)
 
-pq = nBitPrime(20) * nBitPrime(20)
-print(pq)
+bit = 8
+p = nBitPrime(bit)
+q = nBitPrime(bit)
+print("P", p)
+print("Q", q)
+
+
+# pq = nBitPrime(bit) * nBitPrime(bit)
+pq = p*q
+print("pq",pq)
+keys = factor(pq)
+print("P", keys[0], "Q", keys[1])
 
 
 
