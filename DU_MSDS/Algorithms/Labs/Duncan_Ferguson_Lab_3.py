4f7a797e0d82bebb9f1e5ac331c34c93843b3009
Duncan Ferguson <duncan.ferguson@du.edu>
2021-06-27 16:19:05 -0600

    Lab 3 In progress

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_3.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_3.py
@@ -1,1 +1,42 @@
+# Student: Duncan Ferguson
+# Student Id: 871641260
+# Class: Comp 4581
+# Assignment: Lab 2
+# Date 6/22/2021
+
+import random
+
+def isPrime(p):
+    """This function takes a number, p, and returns true if it is prime and false otherwise"""
+    if p > 1:
+        for i in range(2,p,1):
+            if (p % i) == 0:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+def nBitPrime(n):
+    """This is the second Function"""
+    num = random.random()*2**n
+    if num >= 2:
+        print(num)
+    return
+
+
+
+
+def factor(pq):
+    """Som other Function"""
+    return
+
+    # if p == #TODO prime:
+    #     return True
+    # else:
+    #     return False
+
+print(isPrime(11))
+nBitPrime(8)
+
 # Polynomial Curve fit
