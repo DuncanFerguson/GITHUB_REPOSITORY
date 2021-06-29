2060b3506c290276353e956ff7f0261f75038757
Duncan Ferguson <duncan.ferguson@du.edu>
2021-06-29 11:45:18 -0600

    Starting Lab 4

---
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -0,0 +1,19 @@
+# Student: Duncan Ferguson
+# Student Id: 871641260
+# Class: Comp 4581
+# Assignment: Lab 4
+# Date 7/6/2021
+
+# Use a recursive divide-and-concquer algorithm
+# The Algorithm states that the distance between the closet pair of points in a list is a minimum of
+
+# Step 1
+# Divide: Split the list into two equal pieces
+
+# Step 2
+# Conquer: Recursively find the closet pair distance in each sublist (obtains distances 1 and 2 from above
+
+# Step 3 Combine: C
+# Compute the remaing distance (3 from above), and combine them by taking the minimum of the tree
+
+
