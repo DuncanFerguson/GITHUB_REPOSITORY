e99fb3a10abd9512c34dc73fef56fb5494241c35
Duncan Ferguson <duncan.ferguson@du.edu>
2021-06-29 13:49:06 -0600

    Starting Lab 4.

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -16,4 +16,24 @@
 # Step 3 Combine: C
 # Compute the remaing distance (3 from above), and combine them by taking the minimum of the tree
 
+def cPairDist(points):
+    """This function taks in a list of 1-d points (Integers)
+     and returns the distance between the closet pair of points"""
+    points.sort()  # Sorting the list
+    n = len(points)
+    # Dividing Points into mid points
+    half_1 = points[:n//2]
+    half_2 = points[n//2:]
+    print(half_1, "Second", half_2)
+    print(points)
 
+    return points
+
+def recCPairDist(points):
+    """This function takes in a sorted list of points and performs the divide/conqure/combine steps"""
+    return points
+
+A = [7, 4, 12, 14, 2, 10, 16, 6]
+B = [7, 4, 12, 14, 2, 10, 16, 5]
+C = [14, 8, 2, 6, 3, 10, 12]
+cPairDist(C)
