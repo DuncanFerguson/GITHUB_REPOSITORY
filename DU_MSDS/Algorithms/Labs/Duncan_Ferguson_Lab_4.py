f82da7d6f78900bf1e3a8ad46a53f9a97824be47
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-01 11:08:43 -0600

    Starting from the top again

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -16,22 +16,46 @@
 # Step 3 Combine: C
 # Compute the remaing distance (3 from above), and combine them by taking the minimum of the tree
 
+import sys
+INF = sys.maxsize  # Creating Infinity
+
+global min
+min = INF
+
+
+def brute(points):
+    """Brute Force Testing"""
+    min = INF
+    points.sort()
+    for i in range(len(points)-1):
+        if abs(points[i]-points[i+1]) < min:
+            min = abs(points[i]-points[i+1])
+    print(min)
+    return min
+
+
 def cPairDist(points):
     """This function taks in a list of 1-d points (Integers)
      and returns the distance between the closet pair of points"""
     points.sort()  # Sorting the list
     n = len(points)
-    # Dividing Points into mid points
-    half_1 = points[:n//2]
-    half_2 = points[n//2:]
-    print(half_1, "Second", half_2)
-    print(points)
 
-    return points
+    # Dividing Points into two equal lists if they are even. Close to even if they are odd
+    left = points[:n//2]
+    right = points[n//2:]
+    recCPairDist([left, right])
 
 def recCPairDist(points):
-    """This function takes in a sorted list of points and performs the divide/conqure/combine steps"""
-    return points
+    """This function takes in a sorted list of points and performs the divide/ conquer combine steps
+    Returns the min distance it finds."""
+    for i in range(len(points)):
+        if len(points[i]) <= 2:
+            min = brute(points[i])
+        else:
+            cPairDist(points[i])
+    # print(points)
+    return "This"
+
 
 A = [7, 4, 12, 14, 2, 10, 16, 6]
 B = [7, 4, 12, 14, 2, 10, 16, 5]
