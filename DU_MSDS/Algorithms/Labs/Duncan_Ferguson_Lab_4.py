c7fa70e8f9bc251d2df0373a95f923ba872df831
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-01 17:33:08 -0600

    Getting A little Closer

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -6,11 +6,10 @@
 
 import sys
 global min_distance
-min_distance = sys.maxsize  # Creating a min distance to start
 
 def cPairDist(points):
     """This Finds the minimum Distance between a list of points using brute force"""
-
+    min_distance = sys.maxsize
     for i in range(len(points)-1):
         temp = int(abs(points[i+1]-points[i]))
         if temp < min_distance:
@@ -21,15 +20,28 @@ def cPairDist(points):
 def recCPairDist(points):
     """This function takes in a sorted list of points and performs the divide/ conquer combine steps
     Returns the min distance it finds."""
-    return points
+    n = len(points)
+
+    # Base Case
+    if n <= 3:
+        temp = cPairDist(points)
+
+    # Divide
+    left = points[:n//2]
+    right = points[n//2:]
+    print("Left", left)
+    print("Right", right)
+    recCPairDist(left)
+    recCPairDist(right)
+
 
 def main():
     """Main Function to run the code"""
     A = [7, 4, 12, 14, 2, 10, 16, 6]
     B = [7, 4, 12, 14, 2, 10, 16, 5]
     C = [14, 8, 2, 6, 3, 10, 12]
-    print(cPairDist(A))
-
+    A.sort()
+    recCPairDist(A)
 
 
 if __name__ == '__main__':
