17b658c83ee3ddd17f37b832f01d05fea7ce118c
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-01 12:38:16 -0600

    Start From the Top

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -4,12 +4,6 @@
 # Assignment: Lab 4
 # Date 7/6/2021
 
-# Use a recursive divide-and-concquer algorithm
-# The Algorithm states that the distance between the closet pair of points in a list is a minimum of
-
-# Step 1
-# Divide: Split the list into two equal pieces
-
 # Step 2
 # Conquer: Recursively find the closet pair distance in each sublist (obtains distances 1 and 2 from above
 
@@ -17,47 +11,46 @@
 # Compute the remaing distance (3 from above), and combine them by taking the minimum of the tree
 
 import sys
-INF = sys.maxsize  # Creating Infinity
-
-global min
-min = INF
-
 
 def brute(points):
-    """Brute Force Testing"""
-    min = INF
-    points.sort()
+    """Brute Force for finding the min distrance between an array of points in a list that has a been presorted"""
+    min = sys.maxsize
     for i in range(len(points)-1):
-        if abs(points[i]-points[i+1]) < min:
-            min = abs(points[i]-points[i+1])
-    print(min)
+        distance = int(abs(points[i+1]-points[i]))
+        if distance < min:
+            min = distance
     return min
-
 
 def cPairDist(points):
     """This function taks in a list of 1-d points (Integers)
-     and returns the distance between the closet pair of points"""
-    points.sort()  # Sorting the list
-    n = len(points)
+    and returns the distance between the closet pair of points"""
+    return abs(points[0]-points[1])
 
-    # Dividing Points into two equal lists if they are even. Close to even if they are odd
-    left = points[:n//2]
-    right = points[n//2:]
-    recCPairDist([left, right])
 
 def recCPairDist(points):
     """This function takes in a sorted list of points and performs the divide/ conquer combine steps
     Returns the min distance it finds."""
-    for i in range(len(points)):
-        if len(points[i]) <= 2:
-            min = brute(points[i])
-        else:
-            cPairDist(points[i])
-    # print(points)
-    return "This"
+    # Dividing the list into two equal pieces
+    n = len(points)
+    if n > 3:
+        left = points[:n//2]
+        right = points[n//2:]
+        recCPairDist(left)
+        recCPairDist(right)
+    else:
+        print(points)
 
 
-A = [7, 4, 12, 14, 2, 10, 16, 6]
-B = [7, 4, 12, 14, 2, 10, 16, 5]
-C = [14, 8, 2, 6, 3, 10, 12]
-cPairDist(C)
+def main():
+    """Main Function to run the code"""
+    A = [7, 4, 12, 14, 2, 10, 16, 6]
+    # A = [1, 4, 8, 15]
+    # B = [7, 4, 12, 14, 2, 10, 16, 5]
+    # C = [14, 8, 2, 6, 3, 10, 12]
+    A.sort()
+    print(brute(A))
+
+
+if __name__ == '__main__':
+    main()
+
