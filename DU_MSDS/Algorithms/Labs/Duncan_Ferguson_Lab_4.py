bab29d319f6c10d082685720163c889217737149
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-01 13:17:59 -0600

    An answer, pretty sure this is not what we want though

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -13,7 +13,7 @@
 import sys
 
 def brute(points):
-    """Brute Force for finding the min distrance between an array of points in a list that has a been presorted"""
+    """Brute Force for finding the min distance between an array of points in a list that has a been presorted"""
     min = sys.maxsize
     for i in range(len(points)-1):
         distance = int(abs(points[i+1]-points[i]))
@@ -21,34 +21,36 @@ def brute(points):
             min = distance
     return min
 
-def cPairDist(points):
-    """This function taks in a list of 1-d points (Integers)
-    and returns the distance between the closet pair of points"""
-    return abs(points[0]-points[1])
-
 
 def recCPairDist(points):
     """This function takes in a sorted list of points and performs the divide/ conquer combine steps
     Returns the min distance it finds."""
     # Dividing the list into two equal pieces
     n = len(points)
-    if n > 3:
-        left = points[:n//2]
-        right = points[n//2:]
-        recCPairDist(left)
-        recCPairDist(right)
-    else:
-        print(points)
+    left = points[:n//2]
+    right = points[n//2:]
+    mid = [left[-1:][0], right[:1][0]]
+    mid.sort()
+    print("Left: ", left, "\n",
+          "Right: ", right, "\n",
+          "Mid: ", mid)
+    left_min = brute(left)
+    right_min = brute(right)
+    mid_min = brute(mid)
+    combo = [left_min, right_min, mid_min]
+    combo.sort()
+    min = brute(combo)
+    return min
 
 
 def main():
     """Main Function to run the code"""
     A = [7, 4, 12, 14, 2, 10, 16, 6]
-    # A = [1, 4, 8, 15]
+    # A = [1, 4, 9, 15]
     # B = [7, 4, 12, 14, 2, 10, 16, 5]
     # C = [14, 8, 2, 6, 3, 10, 12]
     A.sort()
-    print(brute(A))
+    print(recCPairDist(A))
 
 
 if __name__ == '__main__':
