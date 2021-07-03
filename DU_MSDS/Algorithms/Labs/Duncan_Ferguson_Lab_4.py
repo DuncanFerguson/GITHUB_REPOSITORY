447519c91483a75d77e7f29e7e578bd0d06f0e1e
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-03 09:43:59 -0600

    Lab 4: Complete

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_4.py
@@ -5,11 +5,11 @@
 # Date 7/6/2021
 
 import sys
-global lowest_min
+
 
 def cPairDist(points):
     """This Finds the minimum Distance between a list of points using brute force"""
-    global lowest_min
+    lowest_min = sys.maxsize
     for i in range(len(points)-1):
         temp = int(abs(points[i+1]-points[i]))
         if temp < lowest_min:
@@ -20,45 +20,37 @@ def cPairDist(points):
 def recCPairDist(points):
     """This function takes in a sorted list of points and performs the divide/ conquer combine steps
     Returns the min distance it finds."""
-    global lowest_min
     n = len(points)
     if n <= 3:
-        lowest_min = cPairDist(points)
-        return
+        return cPairDist(points)
 
-    print("Current Min:", lowest_min)
+    # Dividing the points into left right and a middle two
     left = points[:n//2]
-    print("Left", left)
     right = points[n//2:]
-    print("Right", right)
     mid = [left[-1], right[0]]
-    print("Mid", mid)
 
-    recCPairDist(left)
-    recCPairDist(right)
-    recCPairDist(mid)
-    return lowest_min
+    # # Print Statements to check the work
+    # print("Left", left)
+    # print("Right", right)
+    # print("Mid", mid)
+
+    # returning the minimum of a recursive loop for the left, right, mid
+    return min(recCPairDist(left), recCPairDist(right), recCPairDist(mid))
+
 
 def main():
-    """Main Function to run the code"""
-    global lowest_min
+    """Main Function to run the code with the examples that have been given"""
     A = [7, 4, 12, 14, 2, 10, 16, 6]
-    B = [7, 18, 12, 84, 2, 10, 16, 5]
-    C = [14, 8, 2000, 6, 3, 10, 12]
+    B = [7, 4, 12, 14, 2, 10, 16, 5]
+    C = [14, 8, 2, 6, 3, 10, 12]
 
     A.sort()
     B.sort()
     C.sort()
 
-    lowest_min = sys.maxsize
     print(recCPairDist(A))
-
-    lowest_min = sys.maxsize
     print(recCPairDist(B))
-
-    lowest_min = sys.maxsize
     print(recCPairDist(C))
-
 
 
 if __name__ == '__main__':
