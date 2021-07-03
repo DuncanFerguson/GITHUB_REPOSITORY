f251955ab6d4116f75c88c82d83bac69d6f6a226
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-03 12:06:57 -0600

    Lab 5: Queue Getting closer

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_5.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_5.py
@@ -39,9 +39,37 @@ class MyStack(object):
             return self.state[-1]
 
 
+class MyQueue(object):
+    def __init__(self, type):
+        self.elemType = type
+        self.state = []
 
-# class MyQueue(object):
-#     def __init__(self):
+    def __str__(self):
+        """Printing out the state as a string"""
+        string_queue = ""
+        for i in range(len(self.state)):
+            string_queue += " " + str(self.state[i])
+        return string_queue.lstrip()
+
+    def endqueue(self, elem):
+        """Adding an element to the queue"""
+        self.state.append(elem)
+
+    def dequeue(self):
+        """Removing an element from the queue"""
+
+
+    def empty(self):
+        """True iff queue is empty"""
+        return len(self.state) == 0
+
+
+    def top(self):
+        """Returns the top of a nonempty queue"""
+        if self.empty():
+            raise ValueError("Requested top of an empty queue")
+        else:
+            return self.state[-1]
 
 
 def test_stack():
@@ -58,23 +86,23 @@ def test_stack():
     print(s.pop())
     print(s.pop())  # should generate an error
 
-# def test_queue():
-#     """This is for testing the queue"""
-#     q = MyQueue(int)
-#     print(q.empty())
-#     q.enqueue(5)
-#     q.enqueue(8)
-#     print(q.dequeue())
-#     q.enqueue(3)
-#     print(q.empty())
-#     print(q.front())
-#     print(q.dequeue())
-#     print(q.dequeue())
-#     print(q.dequeue())  # should generate an error
+def test_queue():
+    """This is for testing the queue"""
+    q = MyQueue(int)
+    print(q.empty())
+    # q.enqueue(5)
+    # q.enqueue(8)
+    # print(q.dequeue())
+    # q.enqueue(3)
+    # print(q.empty())
+    # print(q.front())
+    # print(q.dequeue())
+    # print(q.dequeue())
+    # print(q.dequeue())  # should generate an error
 
 def main():
-    test_stack()
-    # test_queue()
+    # test_stack()
+    test_queue()
 
 if __name__ == '__main__':
     main()
