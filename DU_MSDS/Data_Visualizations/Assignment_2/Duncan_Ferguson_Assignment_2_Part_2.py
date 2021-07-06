ef45df0db945d26b2979b39ef4481a31dd2b51a0
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-06 17:21:59 -0600

    Assignment 2 Part 2 Complete

--- DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_2.py
+++ DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_2.py
@@ -11,5 +11,27 @@ the IDE names so that they are readable."""
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("py_ide2.csv")
-print(df)
+df = pd.read_csv("py_ide2.csv")  # Reading in the data frame
+
+fig, ax = plt.subplots(1, 2, figsize=(14, 8), num=91)  # Setting up the plot layout
+
+# Horizontal bar Chart
+ax[0].barh(df['IDE'], df['Adoption'])
+
+# Setting up the titles for Horizontal bar Chart
+ax[0].set_title('IDE Adoption')
+ax[0].set_xlabel('Adoption')
+ax[0].set_ylabel('Ide')
+
+# Setting up Vertical Bar Chart
+plt.xticks(range(len(df)), df['IDE'], rotation=65)  # Rotating IDE names to be readable
+ax[1].bar(range(len(df)), df['Adoption'])
+ax[1].set_title("IDE Adoption")
+ax[1].set_xlabel("IDE")
+ax[1].set_ylabel("Adoption")
+
+# Displaying Chart
+plt.tight_layout()
+plt.show()
+
+
