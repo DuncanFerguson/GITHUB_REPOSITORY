d82628c9afd8d531cb6b094a99805fe16a817ca9
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-07 14:30:00 -0600

    Assignment 2 part 4: Part 4 Complete

--- DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_4.py
@@ -12,7 +12,6 @@ on a canvas in two separate panels, one above the other, with the dates aligned.
 import yfinance as yf
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import matplotlib.cbook as cbook
 
 df = yf.download(tickers='DRIP', start='2021-01-01', end='2021-01-31')
 
@@ -20,13 +19,29 @@ fig, ax = plt.subplots(nrows=2, ncols=1)
 
 # Setting Up Close Price Graph
 ax[0].plot(df['Close'], color='r')
-ax[0].set_title("Drip Closing Price")
-ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+ax[0].set_title("'DRIP' Closing Price January 2021")
+
+# Formatting The dates
+ax[0].xaxis.set_major_locator(mdates.MonthLocator())
+ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax[0].xaxis.set_minor_locator(mdates.DayLocator())
+ax[0].xaxis.set_minor_formatter(mdates.DateFormatter('%d'))
+ax[0].tick_params(axis="x", which='minor', rotation=90)
+ax[0].tick_params(which='major', pad=20)
+ax[0].set_ylabel("'$' Closing Price")
 
 # Setting Up Volumes Graph
-ax[1].plot(df['Volume'])
-ax[1].set_title("Drip Volumes")
-ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+ax[1].plot(df['Volume'], color='b')
+ax[1].set_title("'DRIP' Volumes January 2021")
+
+# Formating the Dates
+ax[1].xaxis.set_major_locator(mdates.MonthLocator())
+ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax[1].xaxis.set_minor_locator(mdates.DayLocator())
+ax[1].xaxis.set_minor_formatter(mdates.DateFormatter('%d'))
+ax[1].tick_params(axis="x", which='minor', rotation=90)
+ax[1].tick_params(which='major', pad=20)
+ax[1].set_ylabel("Volume x 1e6")
 
 plt.tight_layout()
 plt.show()
