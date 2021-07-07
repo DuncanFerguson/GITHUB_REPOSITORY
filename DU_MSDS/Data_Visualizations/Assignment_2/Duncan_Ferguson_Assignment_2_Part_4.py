d3c6c313e2f46f3e385cf54f90ebb1b1c0f3e777
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-07 13:55:42 -0600

    Assignment 2 part 4: Can Work. Want to work on setting up the minor ticks

--- DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_4.py
@@ -11,19 +11,22 @@ on a canvas in two separate panels, one above the other, with the dates aligned.
 
 import yfinance as yf
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
 
-df = yf.download('DRIP', '2021-01-01', '2021-01-31')
+df = yf.download(tickers='DRIP', start='2021-01-01', end='2021-01-31')
 
-fig, (ax1, ax2) = plt.subplots(2)
-# fig.suptitle("DRIP Prices and Volumes in January 2021")
+fig, ax = plt.subplots(nrows=2, ncols=1)
 
-ax1.plot(df['Close'])
-ax1.set_title("Drip Closing Price")
+# Setting Up Close Price Graph
+ax[0].plot(df['Close'], color='r')
+ax[0].set_title("Drip Closing Price")
+ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 
-ax2.plot(df['Volume'])
-ax2.set_title("Drip Volumes")
-
-
+# Setting Up Volumes Graph
+ax[1].plot(df['Volume'])
+ax[1].set_title("Drip Volumes")
+ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 
 plt.tight_layout()
 plt.show()
