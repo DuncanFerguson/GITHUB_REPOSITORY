6baba3baab721fa2b6eefd9676fc00002003e468
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-07 15:19:31 -0600

    Assignment 2 part 3: Part 3 Looking Good

--- DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_3.py
+++ DU_MSDS/Data_Visualizations/Assignment_2/Duncan_Ferguson_Assignment_2_Part_3.py
@@ -15,7 +15,7 @@ chart of the same.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+import copy
 
 #  List of eight strings that represent days evenly spread out
 dlist = ['01/01/2021', '02/01/2021', '03/01/2021', '04/01/2021',
@@ -23,10 +23,12 @@ dlist = ['01/01/2021', '02/01/2021', '03/01/2021', '04/01/2021',
 
 # Random uniform distribution, array of eight floats ranging from 100 to 200 value
 ulist = np.random.uniform(low=100, high=200, size=(8,))
+vlist = copy.deepcopy(ulist)
 
 # Turning dlist and ulist into Dataframe
-d = {'date': dlist, 'values': ulist}
-df = pd.DataFrame(d)
+d = list(zip(dlist, vlist))
+df = pd.DataFrame(d, columns=['date', 'values'])
+print(df)
 
 # Converting datestring into datetime object
 df['date'] = pd.to_datetime(df['date'])
@@ -36,20 +38,24 @@ df = df.set_index('date')
 
 # Make two charts 1) a line plot of the values vs. dates and (2) a bar
 # chart of the same.
-fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(14, 8), num=94)
+fig, ax = plt.subplots(nrows=2, ncols=1, sharey='col', sharex='row', figsize=(14, 8), num=94)
 
 # Making the first plot
-ax[0].plot(df['values'], color='r')
+ax[0].plot(df.index, df['values'], color='r')
 ax[0].tick_params(rotation=45)
-ax[0].set_title("Plot of Values versus Date")
+ax[0].set_title("Plot of Values v. Dates")
 ax[0].set_ylabel("Values")
+ax[0].set_axisbelow(True)
+ax[0].grid(axis='y')
 
 # Making Histogram Plot
 ax[1].bar(df.index, df['values'], width=10)
-ax[1].set_title("Bar Chart of Values v Time")
+ax[1].set_title("Bar Chart of Values v. Dates")
 ax[1].set_xlabel('Dates')
+ax[1].set_ylabel("Values")
 ax[1].tick_params(rotation=45)
+ax[1].set_axisbelow(True)
+ax[1].grid(axis='y')
 
 plt.tight_layout()
 plt.show()
-
