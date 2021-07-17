933e364d7a8f1f557aae9f67c2e1790ef15993a2
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-16 18:41:52 -0600

    Preparty commit

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_6.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_6.py
@@ -6,6 +6,10 @@
 
 """"
 https://coderbook.com/@marcus/how-to-create-a-hash-table-from-scratch-in-python/
+https://www.geeksforgeeks.org/hashing-set-3-open-addressing/
+https://www.youtube.com/watch?v=ea8BRGxGmlA
+https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/4_HashTable_2_Collisions/Solution/exercise_hash_table_linear_probing.ipynb
+https://www.youtube.com/watch?v=54iv1si4YCM
 """
 
 
@@ -16,24 +20,40 @@ https://coderbook.com/@marcus/how-to-create-a-hash-table-from-scratch-in-python/
 class MyHashtable(object):
     def __init__(self, size): # Creates an empty hashtable
         self.size = size
-        # TODO Create the list (of size) of empty lists (chaining)
-        self.table = []
-        for i in range(self.size):
-            print(i)
-            self.table.append(['None'])
+        self.table = [[None] for i in range(self.size)]
+        self.status = [['empty'] for i in range(self.size)]  # Create the list (of size) of empty lists (chaining)
 
     def __str__(self):
         return str(self.table)
 
-#
-#     def insert(self, elem): # Adds an element into the hashtable
-#         hash = ord(elem[0]) % self.size
-#         self.table[hash].append(elem)
-#
-#     def member(self, elem): # Returns if element exists in hashtable
-#         hash = ord(elem[0]) % self.size
-#         return elem in self.table[hash]
-#
+    def gethash(self, elem):
+        return ord(elem[0]) % self.size
+
+
+    def insert(self, elem): # Adds an element into the hashtable
+        # TODO this is where the meat of the assignment goes down.
+        hash = ord(elem[0]) % self.size
+        if self.status[hash] is not ['filled']:
+            self.status[hash][0] = 'filled'
+            self.table[hash][0] = elem
+            return
+        else:
+            print("Looping")
+            self.insert(hash+1)
+
+
+
+    def member(self, elem): # Returns if element exists in hashtable
+        hash = ord(elem[0]) % self.size
+        return elem in self.table[hash]
+
+    def delete_member(self, elem):
+        """This function is meant for deleting """
+        return
+
+    def return_status(self):
+        return str(self.status)
+
 # # weneededtomod(%)thehashcode(97)bythetablesize(10)inordertoalwaysproduceanindexthatisinthecorrectrangeforourtable.
 #
 #
@@ -45,9 +65,13 @@ def test_code():
     # Testing code
     s = MyHashtable(10)
     print(s)
-    # s.insert("amy") #97
-    # s.insert("chase") #99
-    # s.insert("chris") #99
+    s.insert("amy") #97
+    print(s, '\n', s.return_status())
+    s.insert("chase") #99
+    print(s,'\n', s.return_status())
+    s.insert("chris") #99
+    print(s, '\n', s.return_status())
+    # print(s)
     # print(s.member("amy"))
     # print(s.member("chris"))
     # print(s.member("alyssa"))
