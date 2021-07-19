581cf89813724ca124d9f4f08c8bd9c19d0408ce
Duncan Ferguson <duncan.ferguson@du.edu>
2021-07-19 17:34:02 -0600

    Data Viz: Assignment 3

---
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_3.py
@@ -0,0 +1,10 @@
+# Student: Duncan Ferguson
+# Student Id: 871641260
+# Class: Comp 4433
+# Assignment: Assignment 2
+# Date 7/22/2021
+
+""" Assignment 3, Part 3: Using the built-in Seaborn dataset car_crashes, prepare plots with
+a scattergram with the linear model for both the total vs. speeding and the total vs.
+alcohol.
+"""
