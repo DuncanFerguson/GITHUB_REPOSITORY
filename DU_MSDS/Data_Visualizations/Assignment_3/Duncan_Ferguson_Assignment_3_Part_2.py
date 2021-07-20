3931a67c4999b4335c01ed4c617c7a42a9ad7c7b
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-20 13:29:40 -0600

    Assignment_3: Part 2: Finished.

--- DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_2.py
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_2.py
@@ -12,17 +12,19 @@ Within that grid, plot the scatterplot for ‘price’ vs. ‘carat’.
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Loading Data Set
 diamonds = sns.load_dataset('diamonds')
-diamonds.info()
 
-# Stripping out Colors D and E and any cut Fair
-# TODO will want to look back through this
+
+# Eliminate colors ‘D’ and ‘E’ as well as the cut ‘Fair’. They are categorial variables
 diamonds = diamonds[(~diamonds.color.isin(["D", "E"])) & (diamonds.cut!='Fair')]
 diamonds["color"] = diamonds["color"].cat.remove_unused_categories()
 diamonds["cut"] = diamonds["cut"].cat.remove_unused_categories()
-diamonds.info()
 
+# Establish a FacetGrid based on ‘cut’ and ‘color’ added hue to be clarity
 sns.set_style('darkgrid')
-g = sns.FacetGrid(diamonds, col='cut', row='color')
-g = g.map(plt.scatter, 'price', 'carat')
+g = sns.FacetGrid(diamonds, col='cut', row='color', hue='clarity')
+
+# Within that grid, plot the scatterplot for ‘price’ vs. ‘carat’
+g = g.map(plt.scatter, 'price', 'carat', edgecolor='w').add_legend()
 plt.show()
