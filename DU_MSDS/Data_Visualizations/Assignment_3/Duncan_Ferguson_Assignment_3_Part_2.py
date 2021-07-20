bf0bacee6292c385dc6ad3618abc31e60f9ea994
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-19 18:47:43 -0600

    Assignment 3: Part 2 Started. Will want to look into cleaning it up

--- DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_2.py
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_2.py
@@ -8,3 +8,21 @@
 FacetGrid based on ‘cut’ and ‘color’. Eliminate colors ‘D’ and ‘E’ as well as the cut ‘Fair’.
 Within that grid, plot the scatterplot for ‘price’ vs. ‘carat’.
 """
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+diamonds = sns.load_dataset('diamonds')
+diamonds.info()
+
+# Stripping out Colors D and E and any cut Fair
+# TODO will want to look back through this
+diamonds = diamonds[(~diamonds.color.isin(["D", "E"])) & (diamonds.cut!='Fair')]
+diamonds["color"] = diamonds["color"].cat.remove_unused_categories()
+diamonds["cut"] = diamonds["cut"].cat.remove_unused_categories()
+diamonds.info()
+
+sns.set_style('darkgrid')
+g = sns.FacetGrid(diamonds, col='cut', row='color')
+g = g.map(plt.scatter, 'price', 'carat')
+plt.show()
