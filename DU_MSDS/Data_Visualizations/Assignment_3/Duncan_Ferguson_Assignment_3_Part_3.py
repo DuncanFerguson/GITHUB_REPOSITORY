0225f7df7ca59b358a68c21115b6cab5e9257dcd
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-19 18:59:26 -0600

    Assignment 3: Part 3 Looking alright. Will want to come back to it and build it out more

--- DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_3.py
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_3.py
@@ -8,3 +8,18 @@
 a scattergram with the linear model for both the total vs. speeding and the total vs.
 alcohol.
 """
+
+# TODO is this right? Will want to check further into it
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+car_crashes = sns.load_dataset('car_crashes')
+car_crashes.info()
+print(car_crashes.head())
+
+sns.scatterplot(x='total', y='speeding', data=car_crashes)
+plt.show()
+
+sns.scatterplot(x='total', y='alcohol', data=car_crashes)
+plt.show()
