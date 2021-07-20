be637b8857e637d643781c3e8c16097af1205c54
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-19 18:36:14 -0600

    Assignment 3: Part 1 Started

--- DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_1.py
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_1.py
@@ -7,5 +7,27 @@
 """Assignment 3, Part 1: Using the built-in Seaborn dataset mpg, provide a heatmap of the
 correlation of all the numeric columns and provide a pairplot of the same."""
 
+"""Useful Linkes
+https://www.youtube.com/watch?v=UgtjatBt3vY
+"""
+
+# TODO look into this on displaying the data differently for the pair plot
+# TODO maybe look at filtering out the objects
+
+import numpy as np
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset('mpg')
+print(df.head())
+df.info()
+
+sns.heatmap(df.corr(), cmap='coolwarm')
+plt.tight_layout()
+plt.show()
+
+sns.pairplot(df)
+plt.tight_layout()
+plt.show()
 
