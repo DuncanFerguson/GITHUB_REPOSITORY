36cf8f090c5fd9cc88538577948ce1572e127974
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-19 19:09:45 -0600

    Assignment 3: Part 4 looking pretty good

--- DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_3/Duncan_Ferguson_Assignment_3_Part_4.py
@@ -9,3 +9,35 @@
 subplots wherein the distribution of each of the numeric columns is presented as a set of
 boxplots, one for each ‘species’.
 """
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# TODO take out the outliers on the box charts
+
+iris = sns.load_dataset('iris')
+iris.info()
+print(iris.head())
+
+# Setting Up subplots
+fig, ax = plt.subplots(2, 2, sharey=True)
+fig.suptitle("Seaborn Iris Data")
+
+# Box Plot 1
+sns.boxplot(ax=ax[0][0], x='sepal_length', y='species', data=iris)
+ax[0][0].set_title("sepal_length")
+
+# Box Plot 2
+sns.boxplot(ax=ax[0][1], x='sepal_width', y='species', data=iris)
+ax[0][1].set_title("sepal_width")
+
+# Box Plot 3
+sns.boxplot(ax=ax[1][0], x='petal_length', y='species', data=iris)
+ax[1][0].set_title("petal_length")
+
+# Box Plot 4
+sns.boxplot(ax=ax[1][1], x='petal_width', y='species', data=iris)
+ax[1][1].set_title("petal_width")
+
+plt.tight_layout()
+plt.show()
