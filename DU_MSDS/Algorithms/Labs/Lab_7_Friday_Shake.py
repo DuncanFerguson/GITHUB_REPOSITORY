ffe1412fe52234922bed69ea41db016f7ed5afce
Duncan <Duncanlockhartferguson@gmail.com>
2021-07-23 16:42:03 -0600

    Getting the wrong amount

--- DU_MSDS/Algorithms/Labs/Lab_7_Friday_Shake.py
+++ DU_MSDS/Algorithms/Labs/Lab_7_Friday_Shake.py
@@ -37,23 +37,23 @@ def DPcoins(coins, amount):
     minCoins[0] = 0
     traceBack[0] = 0
 
-    # for i in range(1, amount+1):
-    #     for coin in coins:
-    #         if coin > i:
-    #             traceBack[i] = traceBack[i]
-    #         elif not minCoins[i] or len(min)
-    #     print("minCoins", minCoins)
-    #     print("traceBack", traceBack)
-
-
     for i in range(1, amount+1):
         for j in range(1, len(coins)+1):
             print("i: ", i, "currentcoin", coins[j-1])
-            if i >= coins[j-1]:
+            if i == coins[j-1]:
+                print("First Hit")
+                minCoins[i] = 1
+                traceBack[i] = coins[j-1]
+            elif i > coins[j-1]:
+                print("Greater Than Hit")
                 numCoins = minCoins[i-coins[j-1]]+1
+                print(numCoins)
+                # traceBack[i] = coins[j-1]
+                traceBack[i] = min(traceBack[i], coins[j-1])
                 if numCoins < minCoins[i]:
                     minCoins[i] = numCoins
-                traceBack[i] = min(coins[j - 1], 1+traceBack[i - coins[j - 1]])
+                    # traceBack = []
+                # traceBack[i] = min(coins[j - 1], 1+traceBack[i - coins[j - 1]])
 
             print("minCoins", minCoins)
             print("traceBack", traceBack)
@@ -62,24 +62,9 @@ def DPcoins(coins, amount):
 def main():
     """Main Program To run the testing.txt code?"""
     C = [1, 5, 10, 12, 25] # coin denominations (must include a penny)
-    # C = [1, 2, 3]
-
-
-    # A = int(input('Enter desired amount of change: '))
     A = 29
     assert A >= 0
-    # print("DAC:")
-    # t1 = time()
-    # numCoins = DACcoins(C, A)
-    # t2 = time()
-    # print("optimal:", numCoins, " in time: ", round((t2-t1)*1000, 1), "ms")
 
-    # # TODO Testing Code
-    # print("count_number_of_ways: ")
-    # t3 = time()
-    # numCoins2 = count_no_of_ways(C, A)
-    # t4 = time()
-    # print("optimal:", numCoins2, " in time: ", round((t4-t3)*1000, 1), "ms")
 
     # TODO THis is testing of the dynamic programing with Traceback
     print("DP:")
