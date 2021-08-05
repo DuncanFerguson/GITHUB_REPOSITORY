91062bd82b4d4ea859894340548af018797a44e5
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-04 19:22:14 -0600

    Assignment 4: Got Boken to start working Need to rewrite the code though

--- DU_MSDS/Data_Visualizations/Assignment_4/Duncan_Ferguson_Assignment_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_4/Duncan_Ferguson_Assignment_4.py
@@ -33,7 +33,10 @@ import geopandas as gpd
 from bokeh.io import curdoc, output_notebook
 from bokeh.models import Slider, HoverTool
 from bokeh.layouts import widgetbox, row, column
-
+from bokeh.palettes import brewer
+from bokeh.io import output_notebook, show, output_file
+from bokeh.plotting import figure
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar
 
 from icecream import ic  # print tester
 
@@ -114,14 +117,38 @@ def main():
             dataset['features'].remove(element)
         else:
             for i in add_df:
-                # TODO maybe add in the state name on this inside property
                 element['properties'][i] = add_df[i][0]
-
 
     # Place the dataset into a GeoPanda
     dataset = gpd.GeoDataFrame(dataset)
     dataset = gpd.GeoDataFrame.from_features(dataset["features"])
-    ic(dataset)
+    ic(dataset.head())
+
+    # TODO REWRITE EVERYTHING BELOW HERE
+    merged_json = json.loads(dataset.to_json())
+    # Convert to String like object.
+    json_data = json.dumps(merged_json)
+
+    # Input GeoJSON source that contains features for plotting.
+    geosource = GeoJSONDataSource(geojson=json_data)  # Define a sequential multi-hue color palette.
+    palette = brewer['YlGnBu'][8]  # Reverse color order so that dark blue is highest obesity.
+    palette = palette[::-1]  # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
+    color_mapper = LinearColorMapper(palette=palette, low=0, high=40)  # Define custom tick labels for color bar.
+    tick_labels = {'0': '0%', '5': '5%', '10': '10%', '15': '15%', '20': '20%', '25': '25%', '30': '30%', '35': '35%',
+                   '40': '>40%'}  # Create color bar.
+    color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=500, height=20,
+                         border_line_color=None, location=(0, 0), orientation='horizontal',
+                         major_label_overrides=tick_labels)  # Create figure object.
+    p = figure(title='Look at US Population Metrics', plot_height=600, plot_width=950, toolbar_location=None)
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None  # Add patch renderer to figure.
+    p.patches('xs', 'ys', source=geosource, fill_color={'field': 'fips', 'transform': color_mapper},
+              line_color='black', line_width=0.25, fill_alpha=1)  # Specify figure layout.
+    # p.add_layout(color_bar, 'below')  # Display figure inline in Jupyter Notebook.
+    # output_notebook()  # Display figure.
+    show(p)
+
+
 
 
     # TODO the start of adding Data Color
