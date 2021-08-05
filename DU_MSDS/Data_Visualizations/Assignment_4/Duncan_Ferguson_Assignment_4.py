735f1ea3c095e27e04387706e494dd8f53493a63
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-04 19:56:47 -0600

    Assignment 4: Got Boken to start working Need to rewrite the code though

--- DU_MSDS/Data_Visualizations/Assignment_4/Duncan_Ferguson_Assignment_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_4/Duncan_Ferguson_Assignment_4.py
@@ -30,16 +30,14 @@ import geoplotlib
 from geoplotlib.colors import ColorMap
 from geoplotlib.utils import BoundingBox
 import geopandas as gpd
-from bokeh.io import curdoc, output_notebook
-from bokeh.models import Slider, HoverTool
-from bokeh.layouts import widgetbox, row, column
+# from bokeh.io import curdoc, output_notebook
+# from bokeh.models import Slider, HoverTool
+# from bokeh.layouts import widgetbox, row, column
 from bokeh.palettes import brewer
 from bokeh.io import output_notebook, show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar
-
 from icecream import ic  # print tester
-
 
 def JSON_Fun():
     """ Aimed at importing the JSON files"""
@@ -123,8 +121,10 @@ def main():
     dataset = gpd.GeoDataFrame(dataset)
     dataset = gpd.GeoDataFrame.from_features(dataset["features"])
     ic(dataset.head())
+    # dataset.plot()
 
-    # TODO REWRITE EVERYTHING BELOW HERE
+    # TODO REWRITE EVERYTHING BELOW HERE This is one route
+
     merged_json = json.loads(dataset.to_json())
     # Convert to String like object.
     json_data = json.dumps(merged_json)
@@ -134,19 +134,15 @@ def main():
     palette = brewer['YlGnBu'][8]  # Reverse color order so that dark blue is highest obesity.
     palette = palette[::-1]  # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
     color_mapper = LinearColorMapper(palette=palette, low=0, high=40)  # Define custom tick labels for color bar.
-    tick_labels = {'0': '0%', '5': '5%', '10': '10%', '15': '15%', '20': '20%', '25': '25%', '30': '30%', '35': '35%',
-                   '40': '>40%'}  # Create color bar.
-    color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=500, height=20,
-                         border_line_color=None, location=(0, 0), orientation='horizontal',
-                         major_label_overrides=tick_labels)  # Create figure object.
     p = figure(title='Look at US Population Metrics', plot_height=600, plot_width=950, toolbar_location=None)
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_color = None  # Add patch renderer to figure.
-    p.patches('xs', 'ys', source=geosource, fill_color={'field': 'fips', 'transform': color_mapper},
+    p.patches('xs', 'ys', source=geosource, fill_color={'field': 'icpsr', 'transform': color_mapper},
               line_color='black', line_width=0.25, fill_alpha=1)  # Specify figure layout.
-    # p.add_layout(color_bar, 'below')  # Display figure inline in Jupyter Notebook.
-    # output_notebook()  # Display figure.
     show(p)
+
+
+
 
 
 
--- DU_MSDS/Data_Visualizations/Assignment_4/Testing.py
+++ DU_MSDS/Data_Visualizations/Assignment_4/Testing.py
@@ -1,98 +1,32 @@
-import json
-import geoplotlib
-from geoplotlib.colors import ColorMap
-from geoplotlib.layers import BaseLayer
-from geoplotlib.core import BatchPainter
-from geoplotlib.colors import colorbrewer
-from geoplotlib.utils import epoch_to_str, BoundingBox, DataAccessObject
-
-
 import pandas as pd
+from bokeh.io import output_notebook
 import icecream as ic
 from datetime import datetime
-
-class TrackLayer(BaseLayer):
-    def __init__(self, dataset, bbox=BoundingBox.WORLD):
-        self.data = dataset
-        self.cmap = colorbrewer(self.data['hex_ident'], alpha=200)
-        self.time = self.data['timestamp'].min()
-        self.painter = BatchPainter()
-        self.view = bbox
-
-    def draw(self, proj, mouse_x, mouse_y, ui_manager):
-        self.painter = BatchPainter()
-        df = self.data.where((self.data['timestamp'] > self.time) & (self.data['timestamp'] <= self.time +180))
-        for element in set(df['hex_ident']):
-            grp = df.where(df['hex_ident'] == element)
-            self.painter.set_color(self.cmap[element])
-            x, y = proj.lonlat_toscreen(grp['lon'], grp['lat'])
-            self.painter.points(x, y, 15, rounded=True)
-        self.time += 1
-        if self.time > self.data['timestamp'].max():
-            self.time = self.data['timestamp'].min()
-        self.painter.batch_draw()
-        ui_manager.info('Current timestamp: {}'.format(epoch_to_str(self.time)))
-
-    def bbox(self):
-        return self.view
-
-def to_epoch(date, time):
-    try:
-        timestamp = round(datetime.strptime('{}{}'.format(date,time), '%Y/%m/%d %H:%M:%S.%f').timestamp())
-        return timestamp
-    except ValueError:
-        return round(datetime.strptime('2017/09/11 17:02:06.418', '%Y/%m/%d %H:%M:%S.%f').timestamp())
+from ipywidgets import interact, interact_manual
 
 
-
-# file = 'National_Obesity_By_State.geojson'
-file = 'flight_tracking.csv'
+file = 'Data-Visualization-with-Python-master\Lesson06\Exercise11\data\stock_prices\stock_prices.csv'
 
 dataset = pd.read_csv(file)
-print(dataset.head())
-print(type(dataset))
+print(dataset)
 
-dataset = dataset.rename(index=str, columns={'latitude': "lat", 'longitude': 'lon'})
+def shorten_time_stamp(timestamp):
+    shortened = timestamp[0]
+    if len(shortened) > 10:
+        parsed_date=datetime.strptime(shortened, '%Y-%m-%d %H:%M:%S')
+        shortened = datetime.strftime(parsed_date, '%Y-%m-%d')
+    return shortened
 
-# Creating a new column called timestamp with the to_epoch method applied
-dataset['timestamp'] = dataset.apply(lambda x: to_epoch(x['date'], x['time']), axis=1)
-
-leeds_bbox = BoundingBox(north=53.8074, west=-3, south=53.7074, east=0)
-
-data = DataAccessObject(dataset)
-geoplotlib.add_layer(TrackLayer(data,  bbox=leeds_bbox))
-geoplotlib.show()
+dataset['short_date'] = dataset.apply(lambda x: shorten_time_stamp(x), axis=1)
 
 print(dataset.head())
 
-# with open(file) as data:
-#     dataset = json.load(data)
-#     first_state = dataset.get('features')[0]
-#     first_state['geometry']['coordinates'] = first_state['geometry']['coordinates'][0][0]
-#     print(json.dumps(first_state, indent=4))
+@interact(Value=False)
+def checkbox(Value=False):
+    print(Value)
 
-# with open(file) as data:
-#     dataset = json.load(data)
-#     states = [feature['properties']['NAME'] for feature in dataset.get('features')]
-#     print(states)
-#
-# def get_color(properties):
-#     return cmap.to_color(properties['Obesity'], maxvalue=40, scale='lin')
-#
-# cmap = ColorMap('Blues', alpha=255, levels=40)
-#
-# geoplotlib.geojson(dataset, fill=True, color=get_color)  # Adding Color to the obesity from get color
-# geoplotlib.geojson(dataset, fill=False, color=[255, 255, 255, 255])  # Filling in the lines as whites
-# geoplotlib.set_bbox(BoundingBox.USA)
-# geoplotlib.tiles_provider('darkmatter')
-# geoplotlib.tiles_provider({
-#     'url':lambda zoom, xtile, ytile:
-#     'http://a.tile.stamen.com/watercolor/%d/%d/%d.png' % (zoom, xtile, ytile),
-#     'tiles_dir':'tiles_dir',
-#     'attribution':'Python Data Visualization | Packt'
-# })
-
-# geoplotlib.tiles_provider('toner-lite')  # Will want to use this for the gray scale printing
-# geoplotlib.show()
-
+options=['Option1', 'Option2', 'Option3', 'Option4']
+@interact(Value=options)
+def slider(Value=options[0]):
+    print(Value)
 
