2d53ec816adfe61805a500a0f9f8d20b2e6ba465
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-16 14:05:37 -0600

    Lab 10: Complete

--- DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_10.py
+++ DU_MSDS/Algorithms/Labs/Duncan_Ferguson_Lab_10.py
@@ -70,56 +70,67 @@ def nQueens(n):
 
 
 def graphColoring(graph, colors):
-    """This is a backgracking Coloring. Depth first search through a space tree.
-    This should create a list of nodes that have been colored so far. Finished when it's reached all the nodes
-    """
+    """This function takes in an adjacency matrix and a list of colors. The function is a depth-first search
+    through a state space tree. This function creates a list of solutions that contains
+    a list of solutions where nodes have been colored."""
+
     initialState = []  # Initial empty state
-    solutions = []
+    solutions_list = []  # Blank Solutions List
     s = MyStack(list)  # For a depth first search
     s.push(initialState)  # Push the initial state onto the Stack
 
-    n = len(graph)
+    n = len(graph)  # Grabbing the number of nodes in the graph
 
     # See if we fond a solved state at a lead node
     # That is, we have filled in every column with a queen
     while not s.empty():
-        currentState = s.pop()
+        currentState = s.pop()  # Grabbing the next state
         currentCol = len(currentState)
+        # See if we have found a solved state at a leaf node
+        # That is, we have filled in every node with a color
         if currentCol == n:
-            solutions.append(currentState)  # Adding a solution to the list
-            # TODO this is where the return is
+            solutions_list.append(currentState)  # Adding a solution to the list and continuing to find more solutions
+            break  # stopping after the first solution, uncomment for all solutions
         else:
-            for color in colors:
+            for color in colors:  # Going through all the different colors
                 feasible = True
                 for node in range(currentCol):
-                    if graph[currentCol][node] and currentState[node] == color:
+                    if graph[currentCol][node] and currentState[node] == color:  # Checking to see if feasible
                         feasible = False
-                        break
                 if feasible:
-                    # Create child by making a copy and appending new col
+                    # Create child by making a copy and appending new color
                     childState = currentState.copy()
                     childState.append(color)
                     s.push(childState)  # Push child onto data structure
-    return solutions[-1]
+    return solutions_list  # Returning a list of the solutions
 
 
 def main():
     """Main Function that runs both nQueens and prints one answer for graphing colors"""
     # Testing code (check 4,5,6,7)
+    print("\nRunning nQueens Test")
     for n in range(4, 5):
-        print("Here")
         nQueens(n)
 
-    # graph = [[False, True, False, False, False, True],
-    #          [True, False, True, False, False, True],
-    #          [False, True, False, True, True, False],
-    #          [False, False, True, False, True, False],
-    #          [False, False, True, True, False, True],
-    #          [True, True, False, False, True, False]]
-    # colors = ['r', 'g', 'b']
-    # print(graphColoring(graph, colors))
+    graph = [[False, True, False, False, False, True],
+             [True, False, True, False, False, True],
+             [False, True, False, True, True, False],
+             [False, False, True, False, True, False],
+             [False, False, True, True, False, True],
+             [True, True, False, False, True, False]]
 
-    # nQueens()
+    colors = ['r', 'g', 'b']
+
+    solution_list = graphColoring(graph, colors)
+    print("\nPrinting out the first Solution")
+    print(solution_list[0])
+
+    # print("\nPrinting last graphColoring Solution")
+    # print(solution_list[-1])  # Printing out the last solution
+    #
+    # print("\nPrinting all the Solutions")
+    # for sol in solution_list:
+    #     print(sol)
 
 
 if __name__ == '__main__':
