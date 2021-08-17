b1f1a08846733bafacefad0c85035a7474804839
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-17 11:18:20 -0600

    Assignment 5 part 5 starting to look better

--- DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_2.py
+++ DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_2.py
@@ -10,3 +10,8 @@ so that a person could see  at a glance that they do nearly cover Africa. Use Sh
 from those three points. Compute the area and perimeter of the triangle assuming flat earth and allowing each
 degree of latitude and each degree of longitude to be considered one unit of length"""
 
+import folium
+
+rabat = [34.020882, -6.841650]
+Cape_town = [-33.918861, 18.423300]
+Mogadishu = [45.318161, 2.046934]
--- DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_5.py
+++ DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_5.py
@@ -14,15 +14,17 @@ import numpy as np
 import pandas as pd
 from bokeh.plotting import figure, show, output_file
 
-output_file('histo.html')
+output_file('histo_Duncan_Ferguson_Assignment_5_Part_5.html')
 np.random.seed(51)
-x = 100 + 15*np.random.randn(500)
+
+n = 500  # Number of Points
+scale = 100
+
+x = np.random.exponential(n, scale)
 xx, edges = np.histogram(x, bins=15, range=[x.min(), x.max()])
 
 df = pd.DataFrame({'xx':xx, 'left': edges[:-1], 'right': edges[1:]})
-
 print(df.head(12))
-
 p = figure(plot_width=1200, plot_height=500)
 p.quad(bottom=0, top=df['xx'], left=df['left'], right=df['right'], fill_color='blue', line_color='black')
 
