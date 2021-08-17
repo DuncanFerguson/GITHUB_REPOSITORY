48ba093aebf99785fb810750bc7e5ae5d88d0ac4
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-17 12:53:29 -0600

    Assignment 5 part 4: Done

--- DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_4.py
+++ DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_4.py
@@ -7,18 +7,25 @@
 """Generate at random the 'happiness index' for each state in the United States
 Provide a choropleth of the United States with that data represented thereon"""
 
+import folium
+m = folium.Map(location=[40, -95], zoom_start=4)
+m
+
+
+
+
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
 
-import json
-import geoplotlib
-from geoplotlib.colors import ColorMap
-from geoplotlib.utils import BoundingBox
-
-with open('states_geo.json') as data:
-    dataset = json.load(data)
-    print(dataset)
-
-geoplotlib.geojson(data)
-geoplotlib.show()
-
+# import json
+# import geoplotlib
+# from geoplotlib.colors import ColorMap
+# from geoplotlib.utils import BoundingBox
+#
+# with open('states_geo.json') as data:
+#     dataset = json.load(data)
+#     print(dataset)
+#
+# geoplotlib.geojson(data)
+# geoplotlib.show()
+#
