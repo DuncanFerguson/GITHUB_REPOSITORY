3ce835ba75799d3d0628dd33da19d348c74ee952
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-16 18:14:44 -0600

    Data Viz Assignment 5 Part 1 Complete

--- DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_1.py
+++ DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_1.py
@@ -7,5 +7,23 @@
 """Using the files capitals_lat_long.csv, form a DataFrame,
 and write out an HTML file that when rendered in a browser displays marker for each capital city"""
 
-# TODO use folium
+import folium
+import pandas as pd
 
+# Importing the CSV file capitals_lat_long.csv
+df = pd.read_csv('capitals_lat_long.csv')
+
+# Printing off head of csv
+print(df.head())
+
+center = [0, 0]
+map = folium.Map(location=center,
+                 tiles='stamenterrain',
+                 zoom_start=3)
+
+for index, row in df.iterrows():
+    folium.Marker([row['Latitude'], row['Longitude']],
+                  popup=row['Country']+": "+row['Capital'],
+                  icon=folium.Icon(icon_color='white')).add_to(map)
+
+map.save('World_Capitals_Assignment_5_Part_1.html')
