9113a55be8f4f32fa47547cff0d49b9d122e95df
Duncan <Duncanlockhartferguson@gmail.com>
2021-08-17 11:45:52 -0600

    Assignment 5 part 5: Done

--- DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_5.py
+++ DU_MSDS/Data_Visualizations/Assignment_5/Duncan_Ferguson_Assignment_5_Part_5.py
@@ -8,25 +8,26 @@
 Choose your own parameters for that use Bokeh to plot a histogram of that distribution such
 that there are at least 15 bins with counts greater than zero"""
 
-# TODO figure out random exponential distribution
-
 import numpy as np
-import pandas as pd
 from bokeh.plotting import figure, show, output_file
 
-output_file('histo_Duncan_Ferguson_Assignment_5_Part_5.html')
-np.random.seed(51)
+np.random.seed(50)
 
+# Setting up 500 points from the random exponential distribution
 n = 500  # Number of Points
 scale = 100
+x = np.random.exponential(scale=scale, size=n)
 
-x = np.random.exponential(n, scale)
-xx, edges = np.histogram(x, bins=15, range=[x.min(), x.max()])
+# Setting up numpy histo
+hist, edges = np.histogram(x, density=True, bins=15, range=[x.min(), x.max()])
+p = figure(title="500 points from the random exponential distribution in 15 bins",
+           x_axis_label="Sample Size",
+           y_axis_label="Sample Distribution")
+p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color='White')
 
-df = pd.DataFrame({'xx':xx, 'left': edges[:-1], 'right': edges[1:]})
-print(df.head(12))
-p = figure(plot_width=1200, plot_height=500)
-p.quad(bottom=0, top=df['xx'], left=df['left'], right=df['right'], fill_color='blue', line_color='black')
-
+# Showing and saving histo
 show(p)
+output_file('histo_Duncan_Ferguson_Assignment_5_Part_5.html')
+
+
 
