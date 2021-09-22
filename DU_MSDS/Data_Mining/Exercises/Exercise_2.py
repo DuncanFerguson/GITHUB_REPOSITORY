12fddc9ad68d5c1b8ad353d9f14d342b61845ff1
Duncan <Duncanlockhartferguson@gmail.com>
2021-09-21 19:40:38 -0600

    In class notes 9.21.21

--- DU_MSDS/Data_Mining/Exercises/Exercise_2.py
+++ DU_MSDS/Data_Mining/Exercises/Exercise_2.py
@@ -1,20 +1,28 @@
 import pandas as pd
 
 df = pd.read_csv('ds_ex2.txt', header=None)
-# df = df.rename(columns={0: "patient id",
-#                         1: "patient height (in meters)",
-#                         2: "patient bmi (body mass index)",
-#                         3: "weight (in kilograms)",
-#                         4: "most recent Idi teset result",
-#                         5: "most recent systolic blood pressure test result"})
+df = df.rename(columns={0: "ID",
+                        1: "HEIGHT",
+                        2: "BMI",
+                        3: "WEIGHT",
+                        4: "LDL",
+                        5: "SBP"})
 
-df_mean_0 = df[0].mean()
-df_mean_1 = df[1].mean()
-df_mean_2 = df[2].mean()
-df_mean_3 = df[3].mean()
-df_mean_4 = df[4].mean()
-df_mean_5 = df[5].mean()
-print(df_mean_0)
+df.to_csv("Excercise_2.csv")
 
 
-print(df)
+# Column 4 Has 90ish missing values
+# Coorelation between BMI and blood preasure
+# Colestoroll
+
+
+# df_mean_0 = df[0].mean()
+# df_mean_1 = df[1].mean()
+# df_mean_2 = df[2].mean()
+# df_mean_3 = df[3].mean()
+# df_mean_4 = df[4].mean()
+# df_mean_5 = df[5].mean()
+# print(df_mean_0)
+#
+#
+# print(df)
