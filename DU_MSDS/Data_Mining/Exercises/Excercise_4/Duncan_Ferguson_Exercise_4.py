f3bdf114343f84c359d5016d2f771b4603ff1124
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-05 20:31:39 -0600

    Start of Excercise 4

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -10,12 +10,14 @@ itemSetCount = collections.defaultdict(int)
 with open('smallinput.csv','r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
+        NUMTRANS += 1
         string = "".join(row)
         perms = [''.join(l) for i in range(len(string)) for l in combinations(string, i + 1)]
         for i in perms:
             itemSetCount[i] += 1
 
-print(itemSetCount)
-dictionary = dict(itemSetCount)
-df = pd.DataFrame(itemSetCount.values(), index=dictionary.keys())
+df = pd.DataFrame({'Keys':list(itemSetCount.keys()), 'Values':list(itemSetCount.values())})
+df.set_index('Keys', inplace=True)
+df['min_sup'] = df['Values']/NUMTRANS * 100
 print(df)
+
