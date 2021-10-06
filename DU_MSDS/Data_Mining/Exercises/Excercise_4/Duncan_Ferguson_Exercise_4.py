9e5f067dd94f4fa346dfe68bfcb34bfb34970dd2
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-05 20:47:54 -0600

    Start of Excercise 4

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -7,7 +7,7 @@ NUMTRANS = 0
 minSupCount = 3
 
 itemSetCount = collections.defaultdict(int)
-with open('smallinput.csv','r') as read_obj:
+with open('smallinput.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
         NUMTRANS += 1
