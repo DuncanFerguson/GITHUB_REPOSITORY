d6c09f33a0e0c7d54dcc87e6c4c574e008d81d5e
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-05 20:29:56 -0600

    Start of Excercise 4

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -1,7 +1,7 @@
 from csv import reader
 import collections
-
-
+from itertools import combinations
+import pandas as pd
 
 NUMTRANS = 0
 minSupCount = 3
@@ -10,15 +10,12 @@ itemSetCount = collections.defaultdict(int)
 with open('smallinput.csv','r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
-        NUMTRANS += 1
-        itemSet = ""
-        for i in range(len(row)):
-            itemSet += row[i]
-        itemSetCount[itemSet] += 1
-
-
-
-
+        string = "".join(row)
+        perms = [''.join(l) for i in range(len(string)) for l in combinations(string, i + 1)]
+        for i in perms:
+            itemSetCount[i] += 1
 
 print(itemSetCount)
-
+dictionary = dict(itemSetCount)
+df = pd.DataFrame(itemSetCount.values(), index=dictionary.keys())
+print(df)
