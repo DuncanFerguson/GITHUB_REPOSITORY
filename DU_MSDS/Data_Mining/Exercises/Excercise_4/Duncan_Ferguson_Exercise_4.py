f04f225b2828c7a0a52799d11026baeb569f3973
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-05 19:30:22 -0600

    Start of Excercise 4

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv("inputExercise4.csv", header=None)
+df = pd.read_csv("inputExercise4.csv", header=None)
 print(df)
+
+# Probably want to use a dictionary
+# Using Brute force going transaction by transaction
 
