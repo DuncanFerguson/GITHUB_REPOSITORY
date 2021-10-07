4e14ec155529f7339261671dc25b8e2acfb0bcf7
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-07 17:09:34 -0600

    Start of Excercise 4

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -4,6 +4,10 @@ from itertools import combinations
 import pandas as pd
 import numpy as np
 
+# https://github.com/vinay-k-pisharody/Apriori-Implementation/blob/master/Apriori.py
+# https://towardsdatascience.com/apriori-association-rule-mining-explanation-and-python-implementation-290b42afdfc6
+
+
 NUMTRANS = 0
 minSupCount = 3
 
@@ -19,6 +23,37 @@ with open('smallinput.csv', 'r') as read_obj:
 
 df = pd.DataFrame({'Keys': list(itemSetCount.keys()), 'Values': list(itemSetCount.values())})
 df.set_index('Keys', inplace=True)
-df['min_sup'] = df['Values']/NUMTRANS * 100
+# df['min_sup'] = df['Values']/NUMTRANS * 100
+df['min_sup'] = df['Values']/NUMTRANS
+
+# Sorting the Pandas
+df['Key'] = df.index
+df['Key_Length'] = df['Key'].str.len()
+df.sort_values(by=["Key_Length", 'Values', 'Key'], ascending=True, inplace=True)
+del df['Key_Length']
+
 print(df)
 
+#%%
+
+columns = list(df['Key'])
+# del df['Key']
+index_i = columns.copy()
+
+df_2 = pd.DataFrame(columns=columns, index=index_i)
+df_2 =df_2.fillna(0)
+
+for row in df_2.index:
+    for c_index in df_2.keys():
+        if row in c_index:
+            df_2.at[row, c_index] = 1
+df_2['Sum'] = df_2.sum(axis=1)
+
+print(df_2)
+#%%
+
+# TODO This equation is most likely wrong
+df["Appearance"] = df_2["Sum"]
+print(df)
+# print(df_2)
+# df_2.to_csv("Testing.csv")
