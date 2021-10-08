d7e0568970b09047931dd276c2cafe719c800477
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-07 20:09:05 -0600

    Excercise 4: Meh

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -8,9 +8,11 @@ import numpy as np
 # https://towardsdatascience.com/apriori-association-rule-mining-explanation-and-python-implementation-290b42afdfc6
 
 NUMTRANS = 0
-minSupCount = 3
+minSupCount = .03
+
 
 itemSetCount = collections.defaultdict(int)
+# with open("inputExercise4.csv", 'r') as read_obj:
 with open('smallinput.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
@@ -24,6 +26,7 @@ df = pd.DataFrame({'Keys': list(itemSetCount.keys()), 'Values': list(itemSetCoun
 df.set_index('Keys', inplace=True)
 # df['min_sup'] = df['Values']/NUMTRANS * 100
 df['min_sup'] = df['Values']/NUMTRANS
+df = df[df.min_sup >= minSupCount]
 
 # Sorting the Pandas
 df['Key'] = df.index
@@ -46,10 +49,12 @@ for row in df_2.index:
     for c_index in df_2.keys():
         perm = [''.join(l) for i in range(len(c_index)) for l in combinations(c_index, i + 1)]
         if row in perm:
-            df_2.at[row, c_index] = df.at[str(c_index), 'Values']
+            df_2.loc[row, c_index] = int(df.at[str(c_index), 'Values']) / int(df.at[str(row), 'Values'])
 
 # df_2.insert(0, "Sum", df_2.sum(axis=1))
 df_2.insert(0, "Values", df["Values"])
 df_2.insert(1, "min_sup", df["min_sup"])
+
+# print(df_2.at["D", "BD"])
 
 print(df_2)
