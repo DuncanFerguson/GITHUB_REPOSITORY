18eda88ecd346727287fed5774dc82ab2358f7e1
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-08 00:18:30 -0600

    Excercise 4: Cleaning up exercise 4v_2 Making the lists

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4_v2.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4_v2.py
@@ -2,8 +2,8 @@
 # Student Id: 871641260
 # Class: Comp 4431-1
 # Assignment: Exercise 4
-# Date: 10/7/2021 <br>
-# Group: Name: Broken Toe <br>
+# Date: 10/7/2021
+# Group: Name: Broken Toe
 # Group Members: Emma Bright, Mike Santoro
 
 from csv import reader
@@ -16,7 +16,6 @@ def main():
     itemSetCount = collections.defaultdict(int)
     NUMTRANS = 0
     minSupCount = .04
-    min_confidence = .5
 
     # Scanning through the file and creating a list of each itemset present and their counts
     with open("inputExercise4.csv", 'r') as read_obj:
@@ -46,6 +45,10 @@ def main():
     df_confidence = pd.DataFrame(columns=df.index, index=df.index)
     df_confidence = df_confidence.fillna(0)  # Filling out the the matrix with zeros
 
+    # Creating list that contain associations that we are looking for
+    minsup_4_minconf_60 = []
+    minsup_4_minconf_50 = []
+
     # Filling out the confidence matrix
     for row in df_confidence.index:
         for c_index in df_confidence.keys():
@@ -53,61 +56,20 @@ def main():
             if row in perm:
                 df_confidence.loc[row, c_index] = int(df.at[str(c_index), 'Values']) / int(df.at[str(row), 'Values'])
 
-    # Adding the the two data frames together
-    df_confidence.insert(0, "Values", df["Values"])
-    df_confidence.insert(1, "min_sup", df["min_support"])
-
-
-    print(df_confidence)
-
-
+                # Appending minimum confidence intervals to a list
+                if df_confidence.at[row, c_index] >= .5 and row != c_index:
+                    minsup_4_minconf_50.append([row, c_index, df_confidence.at[row, c_index]])
+                if df_confidence.at[row, c_index] >= .6 and row != c_index:
+                    minsup_4_minconf_60.append([row, c_index, df_confidence.at[row, c_index]])
 
 
 
-    # # Create associations and test if they satisfy the confidence threshold.
-    # df = df.reindex(columns=df.columns.tolist() + list(itemSetCount.keys()))
-    #
-    # # Adding in the confidence
-    # for row in df.index:
-    #     print(row)
+    print("60", len(minsup_4_minconf_60))
+    print("50", len(minsup_4_minconf_50))
 
 
-    # print(df)
 
 
-    # print(itemSetCount)
-
-    # df = pd.DataFrame({'Keys': list(itemSetCount.keys()), 'Values': list(itemSetCount.values())})
-    # df.set_index('Keys', inplace=True)
-    # df['min_sup'] = df['Values']/NUMTRANS
-    # df = df[df.min_sup >= minSupCount]
-    #
-    # # Sorting the Pandas
-    # df['Key'] = df.index
-    # df['Key_Length'] = df['Key'].str.len()
-    # df.sort_values(by=["Key_Length", 'Values', 'Key'], ascending=True, inplace=True)
-    # del df['Key_Length']
-    #
-    # columns = list(df['Key'])
-    # del df['Key']
-    # index_i = columns.copy()
-    #
-    # df_confidence = pd.DataFrame(columns=columns, index=index_i)
-    # df_confidence =df_confidence.fillna(0)
-    # df_lift = df_confidence.copy()
-    #
-    # for row in df_confidence.index:
-    #     for c_index in df_confidence.keys():
-    #         perm = [''.join(l) for i in range(len(c_index)) for l in combinations(c_index, i + 1)]
-    #         if row in perm:
-    #             df_confidence.loc[row, c_index] = int(df.at[str(c_index), 'Values']) / int(df.at[str(row), 'Values'])
-    #
-    # df["Prob"] = df["Values"] / NUMTRANS
-    #
-    # df_confidence.insert(0, "Values", df["Values"])
-    # df_confidence.insert(1, "min_sup", df["min_sup"])
-    # df_confidence.insert(2, "Prob", df["Prob"])
-    #
     # print("D=>B confidence=", df_confidence.at["D", "BD"], "Lift:", df_confidence.at["BD", "Prob"] / (df_confidence.at["D", "Prob"]*df_confidence.at["B", "Prob"]))
     # print("E=>F confidence=", df_confidence.at["E", "EF"], "Lift:", df_confidence.at["EF", "Prob"] / (df_confidence.at["E", "Prob"]*df_confidence.at["F", "Prob"]))
     # print("CG=>H confidence=", df_confidence.at["CG", "CGH"], "Lift:", df_confidence.at["CGH", "Prob"] / (df_confidence.at["CG", "Prob"]*df_confidence.at["H", "Prob"]))
