b39ddfd81cdd2ac45e83e98dbe26a2b6a7d52bf3
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-07 20:54:51 -0600

    Excercise 4: Confidence

--- DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
+++ DU_MSDS/Data_Mining/Exercises/Excercise_4/Duncan_Ferguson_Exercise_4.py
@@ -4,16 +4,12 @@ from itertools import combinations
 import pandas as pd
 import numpy as np
 
-# https://github.com/vinay-k-pisharody/Apriori-Implementation/blob/master/Apriori.py
-# https://towardsdatascience.com/apriori-association-rule-mining-explanation-and-python-implementation-290b42afdfc6
-
 NUMTRANS = 0
 minSupCount = .03
 
-
 itemSetCount = collections.defaultdict(int)
-# with open("inputExercise4.csv", 'r') as read_obj:
-with open('smallinput.csv', 'r') as read_obj:
+with open("inputExercise4.csv", 'r') as read_obj:
+# with open('smallinput.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
         NUMTRANS += 1
@@ -44,6 +40,7 @@ index_i = columns.copy()
 
 df_2 = pd.DataFrame(columns=columns, index=index_i)
 df_2 =df_2.fillna(0)
+df_3 =df_2.copy()
 
 for row in df_2.index:
     for c_index in df_2.keys():
@@ -51,10 +48,22 @@ for row in df_2.index:
         if row in perm:
             df_2.loc[row, c_index] = int(df.at[str(c_index), 'Values']) / int(df.at[str(row), 'Values'])
 
-# df_2.insert(0, "Sum", df_2.sum(axis=1))
-df_2.insert(0, "Values", df["Values"])
-df_2.insert(1, "min_sup", df["min_sup"])
+for row in df_3.index:
+    for c_index in df_3.keys():
+        df_3.loc[row, c_index] = int(df.at[str(c_index), 'Values']) *NUMTRANS/ int(df.at[str(row), 'Values'])*int(df.at[str(c_index), 'Values'])
+        # print(itemSetCount[c_index])
 
-# print(df_2.at["D", "BD"])
+# for row in df.index:
+#     for col in df.keys():
+#         df_lift.loc[row, col] = df.loc[row, col] * df.at[str(c_index),"Values"]
 
-print(df_2)
+# print(df)
+# print(df_3)
+
+# df_2.insert(0, "Values", df["Values"])
+# df_2.insert(1, "min_sup", df["min_sup"])
+#
+print(df_2.at["D", "BD"])
+print(df_3.at["D", "BD"])
+
+# print(df_2)
