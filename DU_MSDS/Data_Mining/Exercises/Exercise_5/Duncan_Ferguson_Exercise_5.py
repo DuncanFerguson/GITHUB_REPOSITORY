3af5234e7f405b45a7e077c60a75a9f3665332c6
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-12 12:57:18 -0600

    Rename and move

---
+++ DU_MSDS/Data_Mining/Exercises/Exercise_5/Duncan_Ferguson_Exercise_5.py
@@ -0,0 +1,9 @@
+# Student: Duncan Ferguson
+# Student Id: 871641260
+# Class: Comp 4431-1
+# Assignment: Exercise 5
+# Date: 10/16/2021
+# Group: Name: Broken Toe
+# Group Members: Emma Bright, Mike Santoro
+
+
