24fd95c6671d9f4793f5d2a8c593ee8994f35385
Duncan <Duncanlockhartferguson@gmail.com>
2021-10-21 20:03:58 -0600

    Starting Exercise 6 Randomizer Completed

--- DU_MSDS/Data_Mining/Exercises/Exercise_6/Exercise_6.py
+++ DU_MSDS/Data_Mining/Exercises/Exercise_6/Exercise_6.py
@@ -10,6 +10,9 @@ import pandas as pd
 import random
 from sklearn.tree import export_graphviz
 import graphviz
+from sklearn import tree
+from sklearn.tree import export_text
+
 
 def create_tuples():
     """This Function goes through and creates"""
@@ -61,22 +64,64 @@ def create_tuples():
             else:
                 WORKED.append(0)
 
-    print("GPA", GPA)
-    print("RANK", RANK)
-    print("WORKED", WORKED)
+    # print("GPA", GPA)
+    # print("RANK", RANK)
+    # print("WORKED", WORKED)
+    #
 
     # List of lists
     list_o_lists = []
     for num in enumerate(GPA):
         list_o_lists.append([GPA[num[0]], RANK[num[0]], WORKED[num[0]]])
 
-    return list_o_lists
+    graduate = []
+    for person in enumerate(list_o_lists):
+        if sum(person[1]) > 3:
+            graduate.append(1)
+        else:
+            graduate.append(0)
+
+
+    return list_o_lists, graduate
+
+def create_tree(list_o_lists, graduate):
+    """This Function goes through and creates the decision tree"""
+    # X = list_o_lists
+    # Y = graduate
+    # clf = tree.DecisionTreeClassifier()
+    # clf = clf.fit(X,Y)
+    # print(type(clf))
+    # print(clf)
+    Xnum = list_o_lists
+    classifications = graduate
+    clf2 = tree.DecisionTreeClassifier(criterion="entropy")
+    clf2 = clf2.fit(Xnum, classifications)
+
+    print(type(clf2))
+    print(clf2)
+    treeStruct = export_text(clf2)
+    print("\nNow printing export_text(clf2)")
+    print(treeStruct)
+    foo = clf2.apply(Xnum)  # passing in full list of training tuples
+    print("clf2.apply( Xnum ):")
+    print(foo)
+    # now print out line by line as pair:  tuple, which leaf node
+    print("\nNow printing each tuple with the decision tree node it ends up in:")
+    for i in range(len(foo)):
+        print(str(Xnum[i]) + "," + str(foo[i]))
+
+    # NOTE - adding in node_ids and class_names as options to make tree viz more robust
+    dot_data = tree.export_graphviz(clf2, node_ids="true",class_names=('not buy','yes buy'),out_file=None, filled=True, rounded=True, special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.formate = "png"
+    graph.render("graph", view = True)
+
 
 def main():
     """Main Function for running the code"""
-    list_o_list = create_tuples()
-    print(list_o_list)
-
+    list_o_list, graduate = create_tuples()
+    # print(list_o_list)
+    create_tree(list_o_list, graduate)
 
 if __name__ == '__main__':
     main()
