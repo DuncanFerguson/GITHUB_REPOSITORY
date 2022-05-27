da7d4ff9fff9d2698aaf65f43aea4ee22709c258
Duncan <Duncanlockhartferguson@gmail.com>
2022-05-26 20:07:50 -0600

    Inclass push

--- DU_MSDS/Data_Mining/Exercises/Exercise_8/Duncan_Ferguson_handCoded.py
+++ DU_MSDS/Data_Mining/Exercises/Exercise_8/Duncan_Ferguson_handCoded.py
@@ -65,19 +65,6 @@ def main():
     # Selecting a k random clusters
     centroids = df[np.random.choice(df.shape[0], k, replace=False)]
 
-    # If selecting These Points as the centroid errors can occur. These errors don't occur on account of the written
-    # Algorithm. But rather they show weak points within the algorithm.
-
-    # centroids = np.array([[30.92971026, 30.87633269, 32.24465664],
-    #                    [30.96752128, 27.96099865, 30.42075504],
-    #                    [19.00357287, 19.37803004, 19.99621703]])
-
-    # centroids = np.array([[21.171324000344764, 18.40821391520864, 21.44046497631136],
-    #                      [31.181805503563798, 30.392759945491296, 30.52821442244597],
-    #                      [29.664278271579857, 30.650114855130788, 29.576174153798604]])
-    # centroids = np.array([[11.49310608, 10.25651919, 10.54837198],
-    #                       [24.4627228 , 24.21263294, 24.46612038],
-    #                       [ 8.37872705,  8.70543244,  9.90606954]])
 
     # Initializing the base case for the three clusters
     clusters = assign_clusters(centroids, df, k)
