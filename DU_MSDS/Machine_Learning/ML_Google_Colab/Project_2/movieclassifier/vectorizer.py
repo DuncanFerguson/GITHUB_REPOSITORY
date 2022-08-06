a65efefb62b17fb4f519bb27798341687ce72f95
Duncan <Duncanlockhartferguson@gmail.com>
2022-08-06 17:32:32 -0600

    Making moves on a push

--- DU_MSDS/Machine_Learning/ML_Google_Colab/Project_2/movieclassifier/vectorizer.py
+++ DU_MSDS/Machine_Learning/ML_Google_Colab/Project_2/movieclassifier/vectorizer.py
@@ -11,10 +11,8 @@ stop = pickle.load(open(
 
 def tokenizer(text):
     text = re.sub('<[^>]*>', '', text)
-    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
-                           text.lower())
-    text = re.sub('[\W]+', ' ', text.lower()) \
-                   + ' '.join(emoticons).replace('-', '')
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
     tokenized = [w for w in text.split() if w not in stop]
     return tokenized
 
